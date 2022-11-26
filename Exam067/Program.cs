b9edb4bfa0eb950985e345a46a9a3ff177c0d935
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-26 18:52:29 +0300

    принимает на вход число и возвращать сумму его цифр

---
+++ Exam067/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.Write("Введите число: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+Console.WriteLine(SumDigits(n));
+
+/*int SumDigits(int number, int sum = 0)
+{
+    sum += number % 10;
+    number /= 10;
+    if (number > 0)
+    {
+        sum = SumDigits(number, sum);
+    }
+    return sum;
+}*/
+
+int SumDigits(int number)
+{
+    if (number ==0)
+    {
+        return 0;
+    }
+    return number % 10 + SumDigits(number / 10);
+}
